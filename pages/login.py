386c6c448cb93a92931550a9e5fe3ddf69a999de
MJ404-netizen <teemj-wm23@student.tarc.edu.my>
2026-07-17 17:18:18 +0000

    updates

--- pages/login.py
+++ pages/login.py
@@ -148,4 +148,4 @@ with col2:
 col1, col2, col3 = st.columns([1, 1, 1])
 with col2:
     if st.button("← Back to Home", use_container_width=True):
-        st.switch_page("app.py")
+        st.switch_page("main.py")
